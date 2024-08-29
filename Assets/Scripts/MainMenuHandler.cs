ddae930ba154798cf2508977f4db99c1751d1c18
nkase <nicolaskastoris@gmail.com>
2024-08-29 15:20:16 -0400

    Implemented title screen
    Also imported TMPro resources and renamed main scene.

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
