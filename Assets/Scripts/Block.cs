1f76df92e7e383a26f5fbe6de18f62f44bf3e4da
nkase <nicolaskastoris@gmail.com>
2024-09-05 20:26:54 -0400

    Sound pass on interactables

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,10 +8,21 @@ public class Block : Interactable
     //private float weight = 1f;
     [SerializeField]
     private Rigidbody rb;
+    private AudioSource soundEffect;
+
+    private void Start()
+    {
+        soundEffect = GetComponent<AudioSource>();
+    }
 
     public override void Interact(GameObject interactor)
     {
         Debug.Log("Maybe I can push this?");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        soundEffect.Play();
     }
 
     private void OnCollisionStay(Collision collisionInfo)
@@ -25,7 +36,17 @@ public class Block : Interactable
                 //transform.position = Vector3.Lerp(transform.position, Time.deltaTime * speed * contact.normal + transform.position, 1);
                 //contact.otherCollider.attachedRigidbody.AddForce(-contact.normal * , ForceMode.Impulse);
                 rb.AddForce(contact.normal, ForceMode.Impulse);
+                soundEffect.volume = 1;
             }
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.GetComponentInParent<Player>() != null)
+        {
+            soundEffect.volume = 0;
         }
     }
 }
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class Switch : Interactable
 {
-    private bool switchState = false;
+    [SerializeField] private bool switchState = false;
     [SerializeField] private GameObject lever;
     [SerializeField] private Door triggerTarget;
+    private AudioSource soundEffect;
+
+    private void Start()
+    {
+        soundEffect = GetComponent<AudioSource>();
+    }
+
     public override void Interact(GameObject interactor)
     {
         if (switchState == false)
         {
             lever.transform.Rotate(-60, 0, 0);
             triggerTarget.Open();
+            soundEffect.Play();
             switchState = true;
         }
-        else
+        else if (switchState == true)
         {
             lever.transform.Rotate(60, 0, 0);
             triggerTarget.Close();
+            soundEffect.Play();
             switchState = false;
         }
     }
